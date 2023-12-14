7038714d4492b2763b17b492840591315021865d
Yann Caron <cyann74@gmail.com>
2023-12-14 14:01:37 +0100

    change example

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,8 @@ import Grid from "./Grid"
 import { easyGrids } from "./grids"
 
 const grid = Grid.fromString(easyGrids[0])
-console.log(grid.toString());
+//console.log(grid.toString());
 
-Grid.solve(grid)
-console.log(grid.toString());
+console.log('grid candidates', grid.candidates(4, 4));
+
+//console.log(grid.toString());
